f4e74b4544c19f4b6815157c5ba7282f6f495fa2
jenndeprada <depradajennifer@gmail.com>
2023-04-15 23:23:16 -0300

    Modelado de grafico 1

--- scripts/chart_1.js
+++ scripts/chart_1.js
@@ -35,3 +35,132 @@ d3.csv('data/147_desratizacion.csv', d3.autoType).then(data => {
 	})
 	d3.select('#dataviz_1').append(() => dataviz_1)
 })
+
+
+
+
+
+
+// Pregunta a responder: Cuales son los barrios con mas problemas de ratas de la ciudad?
+
+const mapaFetch = d3.json('data/barrios-caba.geojson')
+const dataFetch = d3.dsv(';', 'data/147_desratizacion.csv', d3.autoType)
+
+Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
+  
+  const reclamosPorBarrio = d3.group(data, d => d.domicilio_barrio) // crea un Map
+  console.log(reclamosPorBarrio)
+  /* A cada feature del mapa le agregamos la prop DENUNCIAS */
+  barrios.features.forEach(d => {
+    let nombreBarrio = d.properties.BARRIO
+    let cantReclamos =  reclamosPorBarrio.get(nombreBarrio).length
+    d.properties.DENUNCIAS = cantReclamos
+
+    console.log(nombreBarrio + ': ' + cantReclamos)
+  })
+
+
+  /* Mapa Coroplético */
+  let chartMap = Plot.plot({
+    // https://github.com/observablehq/plot#projection-options
+    projection: {
+      type: 'mercator',
+      domain: barrios, // Objeto GeoJson a encuadrar
+    },
+
+    color: {
+      // Quantize continuo (cant. denuncias) -> discreto (cant. colores)
+      type: 'quantize', 
+      n: 10,
+      scheme: 'ylorbr',
+      label: 'Cantidad de denuncias de ratas',
+      legend: true,
+    },
+
+    marks: [
+      Plot.geo(barrios, {
+        fill: d => d.properties.DENUNCIAS,
+        stroke: 'gray',
+        title: d => `${d.properties.BARRIO}\n${d.properties.DENUNCIAS} denuncias`,
+      }),
+      Plot.text(
+        barrios.features,
+        Plot.centroid({
+          text: (d) => d.properties.BARRIO,
+          fill: "currentColor",
+          stroke: "white",
+          textAnchor: "center",
+          dx: 4,
+          filter: (d) => d.properties.DENUNCIAS > 80
+        })
+      )
+    ],
+  })
+
+  /* Agregamos al DOM la visualización chartMap */
+  d3.select('#dataviz_A').append(() => chartMap)
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+//***************************************************************************************************888 */
+
+
+const locale = {
+	decimal: ',',
+	thousands: '.',
+	grouping: [3],
+}
+
+d3.formatDefaultLocale(locale)
+
+d3.csv('data/147_desratizacion.csv', d3.autoType).then(data => {
+
+		let dataviz_2 = Plot.plot({
+			marks: [
+				Plot.axisX({tickFormat: "", labelAnchor: "center", anchor: "bottom", label: "Año" }),
+				Plot.axisY({anchor: "left", label: "Porcentaje"}),
+				Plot.areaY(newarr, {
+					x: 'anio',
+					y: 'porcentaje',
+					fill: 'status',
+					curve: 'natural',
+					//opacity: 0.5,
+				}),
+				Plot.lineY(newarr, {
+					x: 'anio',
+					y: [50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50],
+					curve: 'natural',
+					opacity: 0.3,
+				}),
+
+			],
+			x: {
+				tickFormat: 'd',
+			},
+			y: {
+				tickFormat: d3.format(',.0f'),
+				grid: true,
+			},
+			color:{
+				range: ["grey", "#EE6D2F"], 
+				type: "categorical",
+				legend: true,
+				//marginLeft: 50,
+			},
+			line: true,
+		})
+		d3.select('#dataviz_2').append(() => dataviz_2)
+	}
+)
+//#endregion
